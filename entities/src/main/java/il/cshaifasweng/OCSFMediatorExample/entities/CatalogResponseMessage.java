f1cba236a0a2f5e364de8d5ae502b14c9218b3ef
bshara <bsharahaj1@gmail.com>
2025-05-26 23:41:38 +0300

    HandleMessageFromClient , almost done + remember to itens abd the database to connect correctly

--- entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/CatalogResponseMessage.java
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/CatalogResponseMessage.java
@@ -5,13 +5,13 @@ import java.io.Serializable;
 import java.util.List;
 
 public class CatalogResponseMessage implements Serializable {
-    private List<Item> items;
+    private List<CatalogItem> items;
 
-    public CatalogResponseMessage(List<Item> items) {
+    public CatalogResponseMessage(List<CatalogItem> items) {
         this.items = items;
     }
 
-    public List<Item> getItems() {
+    public List<CatalogItem> getItems() {
         return items;
     }
 }
