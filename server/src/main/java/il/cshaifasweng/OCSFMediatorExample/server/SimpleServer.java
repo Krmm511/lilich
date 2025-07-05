62e247b8cecd919fef091575b9da53e5678a4b4b
karam <karammahamed123@gmail.com>
2025-07-05 13:36:05 +0300

    karam v2

--- server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/SimpleServer.java
+++ server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/SimpleServer.java
@@ -4,59 +4,53 @@ import il.cshaifasweng.OCSFMediatorExample.server.ocsf.AbstractServer;
 import il.cshaifasweng.OCSFMediatorExample.server.ocsf.ConnectionToClient;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.List;
 
-import il.cshaifasweng.OCSFMediatorExample.entities.Warning;
-import il.cshaifasweng.OCSFMediatorExample.server.ocsf.SubscribedClient;
+import il.cshaifasweng.OCSFMediatorExample.entities.CatalogItem;
+import il.cshaifasweng.OCSFMediatorExample.entities.CatalogDAO; //***to implement***
 
 public class SimpleServer extends AbstractServer {
-	private static ArrayList<SubscribedClient> SubscribersList = new ArrayList<>();
+
+	private final CatalogDAO itemsDB2 = new CatalogDAO();
 
 	public SimpleServer(int port) {
 		super(port);
-		
 	}
 
 	@Override
 	protected void handleMessageFromClient(Object msg, ConnectionToClient client) {
+		if (msg == null) return;
+
 		String msgString = msg.toString();
-		if (msgString.startsWith("#warning")) {
-			Warning warning = new Warning("Warning from server!");
-			try {
-				client.sendToClient(warning);
-				System.out.format("Sent warning to client %s\n", client.getInetAddress().getHostAddress());
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		else if(msgString.startsWith("add client")){
-			SubscribedClient connection = new SubscribedClient(client);
-			SubscribersList.add(connection);
-			try {
-				client.sendToClient("client added successfully");
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
-		}
-		else if(msgString.startsWith("remove client")){
-			if(!SubscribersList.isEmpty()){
-				for(SubscribedClient subscribedClient: SubscribersList){
-					if(subscribedClient.getClient().equals(client)){
-						SubscribersList.remove(subscribedClient);
-						break;
+		String[] parts = msgString.split(" ");
+
+
+
+		try {
+			switch (parts[0]) {
+				case "#catalog" -> {
+					List<CatalogItem> items = itemsDB2.getAllItems();
+					client.sendToClient(items);
+				}
+				case "#item" -> {
+					if (parts.length >= 2) {
+						int itemId = Integer.parseInt(parts[1]);
+						CatalogItem item = itemsDB2.getItemById(itemId);
+						client.sendToClient(item);
+					}
+				}
+				case "#update" -> {
+					if (parts.length >= 3) {
+						int itemId = Integer.parseInt(parts[1]);
+						double newPrice = Double.parseDouble(parts[2]);
+						itemsDB2.updatePrice(itemId, newPrice);
+						List<CatalogItem> updatedItems = itemsDB2.getAllItems();
+						client.sendToClient(updatedItems);
 					}
 				}
 			}
+		} catch (IOException | NumberFormatException e) {
+			e.printStackTrace();
 		}
 	}
-	public void sendToAllClients(String message) {
-		try {
-			for (SubscribedClient subscribedClient : SubscribersList) {
-				subscribedClient.getClient().sendToClient(message);
-			}
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-	}
-
 }
